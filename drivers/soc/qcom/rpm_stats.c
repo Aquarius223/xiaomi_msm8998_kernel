d97bf767ca9143889b74e0bc783ca117222b9443
Aquarius223 <2777968262@qq.com>
2020-10-21 21:42:23 +0800

    treewide: Revert to the old version

--- drivers/soc/qcom/rpm_stats.c
+++ drivers/soc/qcom/rpm_stats.c
@@ -273,11 +273,10 @@ static ssize_t msm_rpmstats_file_read(struct file *file, char __user *bufu,
 	if ((*ppos >= prvdata->len) &&
 		(prvdata->read_idx < prvdata->num_records)) {
 		if (prvdata->platform_data->version == 1)
-			prvdata->len = msm_rpmstats_copy_stats(prvdata); 
-		else if (prvdata->platform_data->version == 2) {
+			prvdata->len = msm_rpmstats_copy_stats(prvdata);
+		else if (prvdata->platform_data->version == 2)
 			prvdata->len = msm_rpmstats_copy_stats_v2(prvdata);
 			*ppos = 0;
-		}
 	}
 	ret = simple_read_from_buffer(bufu, count, ppos,
 			prvdata->buf, prvdata->len);
